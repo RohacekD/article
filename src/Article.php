c6e4131dcd80c22213430608d30258101272c9ab
Dominik Roháček <RohacekD@gmail.com>
2016-12-16 01:27:14 +0100

    Initial commit

---
+++ src/Article.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dominik
+ * Date: 16.12.2016
+ * Time: 1:12
+ */
+
+namespace RohacekD\Article;
+
+use Nette;
+
+
+class Article extends Nette\Application\UI\Control{
+
+}
